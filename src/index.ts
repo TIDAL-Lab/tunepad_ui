3f9b544ef37891ecd275bf1068d0e7f78c4e545f
Michael Horn <michael-horn@northwestern.edu>
2024-06-26 11:18:05 -0500

    added examples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Dial, PianoKeyboard } from "./components";
+import { FilterExample } from "./examples/filter";
 
 customElements.define(Dial.ELEMENT, Dial);
 customElements.define(PianoKeyboard.ELEMENT, PianoKeyboard);
+customElements.define(FilterExample.ELEMENT, FilterExample);
