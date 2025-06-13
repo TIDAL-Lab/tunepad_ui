008f2854c74682b555b12b9f1c4e516d71c16b9a
Michael Horn <michael-horn@northwestern.edu>
2025-06-12 23:02:52 -0500

    added new icons

--- src/icons/icon.ts
+++ src/icons/icon.ts
@@ -12,25 +12,34 @@
  */
 import html from './icon.module.html';
 
+import iconCheck from './icon-check.svg';
 import iconCopy from './icon-copy.svg';
 import iconError from './icon-error.svg';
 import iconLock from './icon-lock.svg';
+import iconMidiRoll from './icon-midiroll.svg'
+import iconMusic from './icon-music.svg';
 import iconPlay from './icon-play.svg';
 import iconRecompile from './icon-recompile.svg';
 import iconTrash from './icon-trash.svg';
-
+import iconWaveform from './icon-waveform.svg';
 
 /**
  * ```html
  * <tunepad-icon icon="trash"></tunepad-icon>
  * ```
  * ### Available Icons
+ * * checkmark
  * * copy
  * * error
  * * lock
+ * * midi
+ * * midiroll
+ * * music
  * * play
  * * recompile
+ * * score
  * * trash
+ * * waveform
  */
 
 export class TunePadIcon extends HTMLElement {
@@ -53,12 +62,19 @@ export class TunePadIcon extends HTMLElement {
     attributeChangedCallback(name : string, oldValue : string, newValue : string) {
         if (name === 'icon' && newValue != oldValue) {
             switch (newValue) {
+                case 'checkmark': this.div.innerHTML = iconCheck; break;
                 case 'copy': this.div.innerHTML = iconCopy; break;
                 case 'error': this.div.innerHTML = iconError; break;
                 case 'lock': this.div.innerHTML = iconLock; break;
+                case 'midi': this.div.innerHTML = iconMidiRoll; break;
+                case 'midiroll': this.div.innerHTML = iconMidiRoll; break;
+                case 'music': this.div.innerHTML = iconMusic; break;
                 case 'play': this.div.innerHTML = iconPlay; break;
                 case 'recompile': this.div.innerHTML = iconRecompile; break;
+                case 'score': this.div.innerHTML = iconMusic; break;
                 case 'trash': this.div.innerHTML = iconTrash; break;
+                case 'waveform': this.div.innerHTML = iconWaveform; break;
+                default: this.div.innerHTML = '';
             }
         }
     }
